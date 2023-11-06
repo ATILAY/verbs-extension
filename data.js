5220bd1785bcd2d8650955cde46d6f55cce3fff0
ysfm <ysfm@jsistem.com>
2023-11-05 19:47:58 -0500

    -

---
+++ data.js
@@ -0,0 +1,53 @@
+window.words = [
+    {
+      "french_verb": "être",
+      "english_verb": "to be",
+      "present": ["suis", "es", "est", "sommes", "êtes", "sont"]
+    },
+    {
+      "french_verb": "avoir",
+      "english_verb": "to have",
+      "present": ["ai", "as", "a", "avons", "avez", "ont"]
+    },
+    {
+      "french_verb": "faire",
+      "english_verb": "to do, to make",
+      "present": ["fais", "fais", "fait", "faisons", "faites", "font"]
+    },
+    {
+      "french_verb": "aller",
+      "english_verb": "to go",
+      "present": ["vais", "vas", "va", "allons", "allez", "vont"]
+    },
+    {
+      "french_verb": "pouvoir",
+      "english_verb": "to be able to",
+      "present": ["peux", "peux", "peut", "pouvons", "pouvez", "peuvent"]
+    },
+    {
+      "french_verb": "voir",
+      "english_verb": "to see",
+      "present": ["vois", "vois", "voit", "voyons", "voyez", "voient"]
+    },
+    {
+      "french_verb": "vouloir",
+      "english_verb": "to want",
+      "present": ["veux", "veux", "veut", "voulons", "voulez", "veulent"]
+    },
+    {
+      "french_verb": "prendre",
+      "english_verb": "to take",
+      "present": ["prends", "prends", "prend", "prenons", "prenez", "prennent"]
+    },
+    {
+      "french_verb": "trouver",
+      "english_verb": "to find",
+      "present": ["trouve", "trouves", "trouve", "trouvons", "trouvez", "trouvent"]
+    },
+    {
+      "french_verb": "donner",
+      "english_verb": "to give",
+      "present": ["donne", "donnes", "donne", "donnons", "donnez", "donnent"]
+    }
+  ]
+  
--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+// Vue.js 2'ye özgü yapılandırma ve instance oluşturma
+new Vue({
+    el: '#app',
+    data: {
+        selected: 0,
+        words: window.words, // Bu 'words' array'ini global scope'ta tanımlamanız gerekir
+    },
+    methods: {
+        getRandomVerb: function() {
 
-async function fetchData() {
-    let randomOrder = Math.floor(Math.random() * 5151); // Random number between 0 and 5150
-
-    console.log(window.engVerbs)
-    // const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    // const record=await res.json();
-    document.getElementById("eng-elem").innerHTML=engVerbs[randomOrder] || "err"
-    // document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-}
-fetchData();
+            this.selected = Math.floor(Math.random() * this.words.length);
+        },
+        cekimFunc(verb, index) {
+            const pronouns = ['je ', 'tu ', 'il/elle ', 'nous ', 'vous ', 'ils/elles '];
+            return pronouns[index] +'<b>'+ verb+'</b>';
+        }
+    }
+});
